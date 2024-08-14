22846f786d422d9eb27034018724bef3d8643868
lobiancovitor <119963373+lobiancovitor@users.noreply.github.com>
2024-08-14 16:25:59 -0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
 IPCA_URL = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.433/dados?formato=json"
 INPC_URL = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.188/dados?formato=json"
 IPC_FIPE_URL = "https://www.idinheiro.com.br/tabelas/tabela-ipc-fipe/"
-IGPM_URL = "https://www.aasp.org.br/produtos-servicos/indices-economicos/mensal/igp-m-fgv/"
-IGPDI_URL = "https://www.aasp.org.br/produtos-servicos/indices-economicos/mensal/igp-di-fgv/"
+IGPM_URL = (
+    "https://www.aasp.org.br/produtos-servicos/indices-economicos/mensal/igp-m-fgv/"
+)
+IGPDI_URL = (
+    "https://www.aasp.org.br/produtos-servicos/indices-economicos/mensal/igp-di-fgv/"
+)
 TR_URL = "https://brasilindicadores.com.br/tr"
 POUPANCA_URL = "https://brasilindicadores.com.br/poupanca/"
